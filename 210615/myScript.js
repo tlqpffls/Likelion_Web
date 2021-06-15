7e6e48bbdc5db0b26bdde21608f5e9cb9215e8ec
tlqpffls <tlqpffls@naver.com>
2021-06-15 15:34:10 +0900

    210615_금일 코드 이동

--- 210615/myScript.js
+++
@@ -1,4 +0,0 @@
-function myFnc() {
-    document.getElementById("demo").innerHTML = "내용 변경";
-    document.getElementById("demo").style.color = "red";
-}
--- 210615/myScript2.js
+++
@@ -1,8 +0,0 @@
-function myFnc1() {
-//    document.getElementById("demo1").innerHTML = "내용 변경1";
-    document.getElementById("demo1").innerHTML = "내용 변경3";
-}
-function myFnc2(text) {
-//    document.getElementById("demo2").innerHTML = "내용 변경2";
-    document.getElementById("demo2").innerHTML = text;
-}
